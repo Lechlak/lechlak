7f200a44a4b38be5dee05a1d13174141f38707df
Andrew Lechlak <AndrewLechlak@gmail.com>
2024-11-01 14:41:12 -0400

    Create index.js

---
+++ mobile-invoice/src/pages/index.js
@@ -0,0 +1,12 @@
+// pages/index.js
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Welcome to My Next.js Site</h1>
+      <p>This is the main landing page of the website.</p>
+      <a href="/about">Learn more about us</a>
+    </main>
+  );
+}
